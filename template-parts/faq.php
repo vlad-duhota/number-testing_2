08aa9c88eff065fae2f10cb23f7df3eccc024197
vlad-duhota <vladarduino@gmail.com>
2022-04-07 15:08:54 +0300

    wp widget and corrected adaptive

--- template-parts/faq.php
+++ template-parts/faq.php
@@ -10,51 +10,37 @@
                         <button type="submit" class="book book_light">Book covid-19 tests</button>
                     </div>
                     <ul class="faq__list wow animate__animated animate__fadeInRight">
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Blandit nibh convallis ut nunc consectetur a orci. Est viverra
+                        <li class="faq-item spoiler">
+                            <h3 class="faq-item__title spoiler__title">Blandit nibh convallis ut nunc consectetur a orci. Est viverra
                                 felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus. Egestas aliquet accumsan amet blandit euismod amet. Sed id
-                                proin amet gravida consequat cursus tellus, nunc. Lacus fringilla sem lobortis ut.
-                                Habitant in ultricies sit dui.
+                            <p class="faq-item__text spoiler__content">
+                            Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis mi accumsan lectus mi lacus. Egestas aliquet accumsan amet blandit euismod amet. Sed id proin amet gravida consequat cursus tellus, nunc. Lacus fringilla sem lobortis ut. Habitant in ultricies sit dui.
                             </p>
                         </li>
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Est viverra felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus.
+                        <li class="faq-item spoiler">
+                            <h3 class="faq-item__title spoiler__title">Est viverra felis leo, libero lorem aler ?</h3>
+                            <p class="faq-item__text spoiler__content">
+                            Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis mi accumsan lectus mi lacus.
                             </p>
                         </li>
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Est viverra felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus.
+                        <li class="faq-item spoiler">
+                            <h3 class="faq-item__title spoiler__title">Purus ultricies sodales velit ac at condimentum parturient lectus ?</h3>
+                            <p class="faq-item__text spoiler__content">
+                            Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis mi accumsan lectus mi lacus.
                             </p>
                         </li>
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Est viverra felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus.
+                        <li class="faq-item spoiler">
+                            <h3 class="faq-item__title spoiler__title">Consectetur pharetra mi risus luctus pulvinar facilisis ?</h3>
+                            <p class="faq-item__text spoiler__content">
+                            Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis mi accumsan lectus mi lacus.
                             </p>
                         </li>
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Est viverra felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus.
+                        <li class="faq-item spoiler">
+                            <h3 class="faq-item__title spoiler__title">Lirut viverra felis leo, libero lorem aler ?</h3>
+                            <p class="faq-item__text spoiler__content">
+                            Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis mi accumsan lectus mi lacus.
                             </p>
-                        </li>
-                        <li class="faq-item">
-                            <h3 class="faq-item__title">Est viverra felis leo, libero lorem aler ?</h3>
-                            <p class="faq-item__text">
-                                Fusce ut in pellentesque massa neque, pulvinar donec. Tortor aliquam ipsum quisque felis
-                                mi accumsan lectus mi lacus.
-                            </p>
-                        </li>
+                        </li>   
                     </ul>
                 </div>
             </section>
